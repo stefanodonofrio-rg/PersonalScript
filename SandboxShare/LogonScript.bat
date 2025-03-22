3dc94d65e875d549e055de0248558f5177eccc11
stefanodonofrio-rg <Stefano.D'Onofrio@red-gate.com>
2025-03-22 07:54:10 +0000

    Add Sandbox Configuration

---
+++ SandboxShare/LogonScript.bat
@@ -0,0 +1,4 @@
+curl -L "https://update.code.visualstudio.com/latest/win32-x64-user/stable" --output C:\users\WDAGUtilityAccount\Downloads\vscode.exe
+
+C:\users\WDAGUtilityAccount\Downloads\vscode.exe /verysilent /suppressmsgboxes
+    
